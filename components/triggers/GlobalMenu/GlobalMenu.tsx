0892caa9fcd146233c58854f0d20d5f1982936d7
Keisuke Tanaka <keisuketanaka97@gmail.com>
2022-11-02 06:18:51 +0100

    fix pipeline

--- components/triggers/GlobalMenu/GlobalMenu.tsx
+++ components/triggers/GlobalMenu/GlobalMenu.tsx
@@ -6,12 +6,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { toggleGlobalMenuIsShown } from '../../../store/reducers/ui.reducer';
 import { AppState } from '../../../store';
 
-export const GlobalMenu = ({ className }: { className: string }) => {
+export const GlobalMenu = ({ className }: { className?: string }) => {
   const isShown = useSelector<AppState>(state => state.ui.globalMenu.isShown);
   const dispatch = useDispatch();
   return (
     <>
-      <nav className={`fixed z-40 rounded-lg border-2 border-black bg-dark-purple p-2 duration-100 portrait:top-10 portrait:right-6 landscape:top-16 landscape:right-24 ${isShown ? s.shown : s.hidden} ${className}`}>
+      <nav className={`fixed z-40 rounded-lg border-2 border-black bg-dark-purple p-2 duration-100 portrait:top-10 portrait:right-6 landscape:top-16 landscape:right-24 ${isShown ? s.shown : s.hidden} ${className ?? ''}`}>
         <ul className='flex flex-col gap-5 rounded-lg border-2 border-army-green bg-white px-5 py-8'>
           <li>
             <Link className={s.nav} href='/blog'>
