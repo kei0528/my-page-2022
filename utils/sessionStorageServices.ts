1c9bd8f11e355fca86af483313ad4f666761509d
Keisuke Tanaka <keisuketanaka97@gmail.com>
2022-11-11 23:40:27 +0100

    fix undefined error

--- utils/sessionStorageServices.ts
+++ utils/sessionStorageServices.ts
@@ -1,17 +1,17 @@
 export const sessionStorageServices = {
   set: (key: string, value: any) => {
-    if (!typeof sessionStorage) return;
+    if (typeof window === 'undefined') return;
     sessionStorage.setItem(key, JSON.stringify(value));
   },
   get: (key: string) => {
-    if (!typeof sessionStorage) return;
+    if (typeof window === 'undefined') return;
     const data = sessionStorage.getItem(key);
     if (data === 'true') return true;
     if (data === 'false') return false;
     return data as any;
   },
   delete: (key: string) => {
-    if (!typeof sessionStorage) return;
+    if (typeof window === 'undefined') return;
     sessionStorage.removeItem(key);
-  }
+  },
 };
