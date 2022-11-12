758a4c1e152031b00458674d16676999736b9894
Keisuke Tanaka <keisuketanaka97@gmail.com>
2022-11-12 23:30:15 +0100

    test

--- components/pages/Blog/Blog.tsx
+++ components/pages/Blog/Blog.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 // import fs from 'fs';
 // import path from 'path';
 // import matter from 'gray-matter';
@@ -5,7 +7,8 @@ import { BaseMainLayout } from '../../uis/BaseMainLayout';
 import { CardLandscape } from '../../uis/CardLandscape';
 import { Header } from '../../uis/Header';
 import { SearchBar } from '../../uis/SearchBar';
-import { urls } from '../../../statics/urls';
+// import { urls } from '../../../statics/urls';
+import useSWR from 'swr';
 
 type BlogPostType = {
   slug: string;
@@ -17,14 +20,22 @@ type BlogPostType = {
   };
 }[];
 
-async function getBlogPosts() {
-  const res = await fetch(urls.baseUrl + '/api/blog');
-  const data = await res.json();
-  return data as BlogPostType;
-}
+// async function getBlogPosts() {
+//   const res = await fetch(urls.baseUrl + '/api/blog');
+//   const data = await res.json();
+//   return data as BlogPostType;
+// }
 
-const Blog = async () => {
-  const posts = await getBlogPosts();
+const Blog = () => {
+  const fetcher = async (url: string) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data as BlogPostType;
+  };
+
+  const { data } = useSWR('/api/blog', fetcher);
+  // const posts = await getBlogPosts();
+  const posts = data;
   console.log('posts', posts);
 
   return (
@@ -33,9 +44,7 @@ const Blog = async () => {
       <BaseMainLayout>
         <SearchBar className="max-w-480px mx-auto mt-[-64px] w-2/3 min-w-[260px]" placeholder="Search" />
         <div className="my-14 flex flex-col gap-3">
-          {posts.map((post) => (
-            <CardLandscape key={post.slug} {...post.frontmatter} slug={post.slug} />
-          ))}
+          {posts && posts.map((post) => <CardLandscape key={post.slug} {...post.frontmatter} slug={post.slug} />)}
         </div>
       </BaseMainLayout>
     </>
