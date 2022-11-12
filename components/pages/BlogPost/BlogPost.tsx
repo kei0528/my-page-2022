91a85a3f069b0392f4815f9332b87fdc66ce2976
Keisuke Tanaka <keisuketanaka97@gmail.com>
2022-11-12 20:37:38 +0100

    change height

--- components/pages/BlogPost/BlogPost.tsx
+++ components/pages/BlogPost/BlogPost.tsx
@@ -15,7 +15,7 @@ async function getPost({ slug }: { slug: string }) {
   return {
     frontmatter,
     content,
-    slug
+    slug,
   };
 }
 
@@ -26,14 +26,22 @@ const BlogPost = async ({ params }: { params?: any }) => {
     <>
       <Header bgImg={frontmatter.thumbnail} />
       <BaseMainLayout>
-        <article className='relative z-10 -mt-36 mb-16 min-h-[320px] rounded-2xl bg-white px-5 pt-8 pb-10 shadow-lg duration-300 hover:-translate-y-1 hover:shadow-xl sm:mx-auto sm:w-11/12 sm:px-8 sm:pt-12 sm:pb-16 md:w-10/12'>
-          <Link className='mb-9 flex w-fit gap-2 text-sm text-black duration-200 hover:-translate-y-1 md:text-base' href='/v1/blog'>
-            <Image src='/assets/icons/Icon_Arrow-left.svg' width={16} height={16} alt='' />
+        <article className="relative z-10 -mt-36 mb-16 min-h-[320px] rounded-2xl bg-white px-5 pt-8 pb-10 shadow-lg duration-300 hover:-translate-y-1 hover:shadow-xl sm:mx-auto sm:w-11/12 sm:px-8 sm:pt-12 sm:pb-16 md:w-10/12">
+          <Link
+            className="mb-9 flex w-fit gap-2 text-sm text-black duration-200 hover:-translate-y-1 md:text-base"
+            href="/v1/blog"
+          >
+            <Image src="/assets/icons/Icon_Arrow-left.svg" width={16} height={24} alt="" />
             Back
           </Link>
-          <time className='font-game text-3xl leading-game-md text-black md:text-game-md'>{frontmatter.date}</time>
-          <h1 className='mt-2 mb-5 text-3xl font-bold tracking-wide text-gb-purple md:mt-2 md:mb-9 md:text-5xl'>{frontmatter.title}</h1>
-          <div className={`text-lg leading-6 md:text-xl md:leading-7 ${s.template_html}`} dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+          <time className="font-game text-3xl leading-game-md text-black md:text-game-md">{frontmatter.date}</time>
+          <h1 className="mt-2 mb-5 text-3xl font-bold tracking-wide text-gb-purple md:mt-2 md:mb-9 md:text-5xl">
+            {frontmatter.title}
+          </h1>
+          <div
+            className={`text-lg leading-6 md:text-xl md:leading-7 ${s.template_html}`}
+            dangerouslySetInnerHTML={{ __html: marked(content) }}
+          ></div>
         </article>
       </BaseMainLayout>
     </>
