9322a318277500ea05aacf91edcfac5f97179499
Keisuke Tanaka <keisuketanaka97@gmail.com>
2022-12-31 23:05:04 +0100

    apply device dark-mode setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 import 'modern-css-reset';
 import '../styles/globals.scss';
 import '../styles/generics.scss';
@@ -6,29 +6,16 @@ import { SideNav } from '../components/triggers/SideNav';
 import { GlobalMenu } from '../components/triggers/GlobalMenu';
 import { Provider } from '../components/triggers/Provider';
 import { isDarkmode } from '../statics/conditions';
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Analytics } from '../components/triggers/Analytics';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  // useEffect(() => {
-  //   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  //     document.querySelector('html')!.classList.add('dark');
-  //   }
+  useEffect(() => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.querySelector('html')!.classList.add('dark');
+    }
+  }, []);
 
-  //   const changeVhCssVar = () => {
-  //     document.documentElement.style.setProperty('--real-max-vh', window.innerHeight + 'px');
-  //     document.documentElement.style.setProperty('--real-max-vh-with-menu', window.innerHeight - 60 + 'px');
-  //   };
-  //   changeVhCssVar();
-
-  //   window.addEventListener('resize', () => {
-  //     changeVhCssVar();
-  //   });
-
-  //   return () => {
-  //     window.removeEventListener('resize', changeVhCssVar);
-  //   };
-  // }, []);
   return (
     <html lang="en" data-lt-installed={true} className={isDarkmode ? 'dark' : ''}>
       <head>
