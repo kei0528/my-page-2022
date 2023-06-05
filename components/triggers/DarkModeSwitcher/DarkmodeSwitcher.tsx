256c25a25a17d32981ff98c3ad3dfe440d4071a9
Keisuke Tanaka <keisuketanaka97@gmail.com>
2023-06-05 14:41:57 +0200

    remove logger

--- components/triggers/DarkModeSwitcher/DarkmodeSwitcher.tsx
+++ components/triggers/DarkModeSwitcher/DarkmodeSwitcher.tsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 
 export const DarkModeSwitcher = () => {
   useEffect(() => {
-    console.log('toggled!');
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       document.querySelector('html')!.classList.add('dark');
     }
